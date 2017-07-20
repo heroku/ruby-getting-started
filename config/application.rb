198534a57f687ce21a0a1aa94dfd47d515755775
schneems <richard.schneeman@gmail.com>
2017-07-20 09:19:44 -0500

    add trashed

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'trashed/railtie'
+require 'statsd'
+
+
 module RubyGettingStarted
   class Application < Rails::Application
+    config.trashed.statsd = Statsd.new('localhost', ENV["PORT"]) if ENV["PORT"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
