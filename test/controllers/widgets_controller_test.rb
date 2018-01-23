907ac8d4e908e1da396c30f9efda7a0e3e7a66ec
Caleb Thompson <caleb@calebthompson.io>
2018-01-23 14:09:53 -0600

    Update to Rails 5.1.4
    Unlock other gems while we're at it, so that we can provide customers
    with the most up-to-date, featureful, and importantly secure versions.

--- test/controllers/widgets_controller_test.rb
+++ test/controllers/widgets_controller_test.rb
@@ -8,7 +8,6 @@ class WidgetsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:widgets)
   end
 
   test "should get new" do
@@ -18,30 +17,45 @@ class WidgetsControllerTest < ActionController::TestCase
 
   test "should create widget" do
     assert_difference('Widget.count') do
-      post :create, widget: { description: @widget.description, name: @widget.name, stock: @widget.stock }
+      post :create,
+        params: {
+          widget: {
+            description: @widget.description,
+            name: @widget.name,
+            stock: @widget.stock
+          }
+        }
     end
 
-    assert_redirected_to widget_path(assigns(:widget))
+    assert_redirected_to widget_path(@widget.id + 1)
   end
 
   test "should show widget" do
-    get :show, id: @widget
+    get :show, params: { id: @widget }
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @widget
+    get :edit, params: { id: @widget }
     assert_response :success
   end
 
   test "should update widget" do
-    patch :update, id: @widget, widget: { description: @widget.description, name: @widget.name, stock: @widget.stock }
-    assert_redirected_to widget_path(assigns(:widget))
+    patch :update,
+      params: {
+        id: @widget,
+        widget: {
+          description: @widget.description,
+          name: @widget.name,
+          stock: @widget.stock
+        }
+      }
+    assert_redirected_to widget_path(@widget)
   end
 
   test "should destroy widget" do
     assert_difference('Widget.count', -1) do
-      delete :destroy, id: @widget
+      delete :destroy, params: { id: @widget }
     end
 
     assert_redirected_to widgets_path
