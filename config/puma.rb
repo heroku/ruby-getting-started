209c6167bc1f027eab3a5e7e7a282045cfc3c185
Richard Schneeman <richard.schneeman+foo@gmail.com>
2023-03-07 11:31:40 -0600

    Remove unneeded config (#116)
    This connection logic hasn't been required since Rails ~4.1. We can remove it from this Rails 7 app.

--- config/puma.rb
+++ config/puma.rb
@@ -7,11 +7,5 @@ preload_app!
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
-
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
