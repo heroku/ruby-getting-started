9d627dd7db215a6aec81b2ecb14db49bd54b5efd
Schneems <richard.schneeman+foo@gmail.com>
2025-07-30 09:53:01 -0400

    Update keepalive timeout with Router 2.0 settings
    Router 2.0 introduced keepalive support. It will close connections idle after 90 seconds https://devcenter.heroku.com/articles/http-routing#keepalives. We want to avoid a situation where they send a request right before puma closes the connection. We can do that by setting it to the same timeout the router uses + some value (such as 5 seconds). 
    
    This timeout is used in the reactor when buffering unfinished request bodies and waiting for new requests on a still open client connection. If a connection is closed incorrectly and doesn't send a TCP `FIN` this timeout will prevent puma holding onto that connection forever.
    
    There is currently a problem in puma 6x and prior with keepalives that causes high request variance https://github.com/puma/puma/issues/3487. So it's recommended that puma users disable keepalive connections by until that problem is fixed upstream:
    
    ```
    $ heroku labs:enable http-disable-keepalive-to-dyno
    ```

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,9 @@ workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
+# Router keepalive idle timeout + 5 seconds
+persistent_timeout(95)
+
 preload_app!
 
 # Support IPv6 by binding to host `::` instead of `0.0.0.0`
