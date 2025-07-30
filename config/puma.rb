4f57f3ffd6e5fa3061c8d87382fa0d92a9678f05
Schneems <richard.schneeman+foo@gmail.com>
2025-07-30 10:36:16 -0400

    Disable keepalives by default
    This matches the current devcenter docs https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server.

--- config/puma.rb
+++ config/puma.rb
@@ -2,10 +2,14 @@ workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
+preload_app!
+
 # Router keepalive idle timeout + 5 seconds
 persistent_timeout(95)
 
-preload_app!
+# Turn off keepalive support for better long tails response time with Router 2.0
+# Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
+enable_keep_alives(false) if respond_to?(:enable_keep_alives)
 
 # Support IPv6 by binding to host `::` instead of `0.0.0.0`
 port(ENV.fetch("PORT") { 3000 }, "::")
