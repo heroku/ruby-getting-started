3173689a105e9a24a03a8039a876762c448a4a12
Schneems <richard.schneeman+foo@gmail.com>
2025-09-08 16:49:08 -0500

    Prefer parens

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
-workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+workers(ENV.fetch('WEB_CONCURRENCY') { 2 })
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
-threads threads_count, threads_count
+threads(threads_count, threads_count)
 
 preload_app!
 
@@ -13,7 +13,7 @@ enable_keep_alives(false) if respond_to?(:enable_keep_alives)
 
 # Support IPv6 by binding to host `::` instead of `0.0.0.0`
 port(ENV.fetch("PORT") { 3000 }, "::")
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment(ENV.fetch("RAILS_ENV") { "development" })
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+plugin(:tmp_restart)
