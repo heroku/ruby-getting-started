c9c13218c7a02e608de439ef3f7def2a140733c3
Schneems <richard.schneeman+foo@gmail.com>
2025-09-08 16:50:43 -0500

    Update doc comments

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,11 @@
+# Processes count, allows better CPU utilization when executing Ruby code.
 workers(ENV.fetch('WEB_CONCURRENCY') { 2 })
+
+# Thread per process count, allows context switching on IO bound tasks for better CPU utilization.
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads(threads_count, threads_count)
 
+# Reduce memory usage on copy on write (CoW) systems.
 preload_app!
 
 # Router keepalive idle timeout + 5 seconds
@@ -11,9 +15,9 @@ persistent_timeout(95)
 # Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
 enable_keep_alives(false) if respond_to?(:enable_keep_alives)
 
-# Support IPv6 by binding to host `::` instead of `0.0.0.0`
+# Support IPv6 by binding to host `::` instead of `0.0.0.0`.
 port(ENV.fetch("PORT") { 3000 }, "::")
 environment(ENV.fetch("RAILS_ENV") { "development" })
 
-# Allow puma to be restarted by `rails restart` command.
+# Allow puma to be restarted by `rails restart` command locally.
 plugin(:tmp_restart)
