e2f1f4d9f30bdb2023aa49fce77106e67117a576
Richard Schneeman <richard.schneeman+foo@gmail.com>
2025-09-09 13:18:24 -0500

    Update Puma version and config (#196)
    * Prefer parens
    
    * Update doc comments
    
    * Use default PUMA_PERSISTENT_TIMEOUT for keepalive
    
    - https://github.com/heroku/heroku-buildpack-ruby/pull/1641
    - https://github.com/heroku/buildpacks-ruby/pull/454
    
    * Extract `rails_env` into a variable
    
    * Use secure port locally
    
    When you're running in development mode you don't want to be reachable via the open internet.
    
    * Update Puma version

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,38 @@
-workers ENV.fetch('WEB_CONCURRENCY') { 2 }
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
-threads threads_count, threads_count
+# Processes count, allows better CPU utilization when executing Ruby code.
+workers(ENV.fetch('WEB_CONCURRENCY') { 2 })
 
+# Thread per process count, allows context switching on IO bound tasks for better CPU utilization.
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads(threads_count, threads_count)
+
+# Reduce memory usage on copy on write (CoW) systems.
 preload_app!
 
-# Router keepalive idle timeout + 5 seconds
-persistent_timeout(95)
+# Ruby buildpack sets RAILS_ENV in production.
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+environment(rails_env)
 
-# Turn off keepalive support for better long tails response time with Router 2.0
-# Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
-enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+# Support IPv6 by binding to host `::` in production instead of `0.0.0.0` and `::1` instead of `127.0.0.1` in development.
+host = rails_env == "production" ? "::" : "::1"
 
-# Support IPv6 by binding to host `::` instead of `0.0.0.0`
-port(ENV.fetch("PORT") { 3000 }, "::")
-environment ENV.fetch("RAILS_ENV") { "development" }
+# PORT environment variable is set by Heroku in production.
+port(ENV.fetch("PORT") { 3000 }, host)
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# Allow puma to be restarted by `rails restart` command locally.
+plugin(:tmp_restart)
+
+# Heroku strongly recommends upgrading to Puma 7+. If you cannot upgrade,
+# please see the Puma 6 and prior configuration section below.
+#
+# Puma 7+ already supports PUMA_PERSISTENT_TIMEOUT natively. Older Puma versions set:
+#
+# ```
+# persistent_timeout(ENV.fetch("PUMA_PERSISTENT_TIMEOUT") { 95 }))
+# ```
+#
+# Puma 7+ fixes a keepalive issue that affects long tails response time with Router 2.0.
+# Older Puma versions set:
+#
+# ```
+# enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+# ```
