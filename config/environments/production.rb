c1f385d28bdf550245631fbe011e33c711c7e48c
Schneems <richard.schneeman+foo@gmail.com>
2026-06-01 11:13:16 -0500

    Disable Active Storage variant processor
    Before (git push, rake db:migrate, and heroku logs):
    
      Generating image variants require the image_processing gem. Please
      add `gem "image_processing", "~> 1.2"` to your Gemfile or set
      `config.active_storage.variant_processor = :disabled`.
    
    After: warning no longer appears.
    
    The app doesn't process images, so variant support is unnecessary.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_storage.variant_processor = :disabled
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
